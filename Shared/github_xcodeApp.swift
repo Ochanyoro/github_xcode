2fa42472d3330b90ed607021d7172196334c451d
Ochanyoro <wadaoo0223@icloud.com>
2022-06-15 19:14:30 +0900

    Initial Commit

---
+++ Shared/github_xcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_xcodeApp.swift
+//  Shared
+//
+//  Created by 大和田一裕 on 2022/06/15.
+//
+
+import SwiftUI
+
+@main
+struct github_xcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
